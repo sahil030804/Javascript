471af12b88f8c4c3aa53f5079b6f51943cdd30da
sahil030804 <sahilranpariya60@gmail.com>
2024-08-02 10:17:04 +0000

    save

---
+++ Basic_01/datatypes.js
@@ -0,0 +1,18 @@
+let name = "sahil"
+let cno = 9016120415
+let city = "surat"
+let state;
+
+console.log(city);
+
+// number => 2 to power 53 max value
+//bigint => large websites like stock market or reddit
+//string => " "
+//boolean => true / false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof true);
---
+++ Basic_01/variables.js
@@ -0,0 +1,13 @@
+const accountID = 12345
+let accountEmail = "sahil@gmail.com"
+var accountPassword = "1234"
+accountCity = "surat"
+
+// accountID = 234
+
+accountEmail ="abc@gmail.com"
+accountPassword = "abcd"
+accountCity = "vapi"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity]);
