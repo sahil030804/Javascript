2a7db4af6374c577d05f630bf80c08ba73435a1e
sahil030804 <sahilranpariya60@gmail.com>
2024-08-05 13:20:31 +0000

    functions

---
+++ Basic_03/functions.js
@@ -0,0 +1,55 @@
+function sayMyname() {
+  console.log("S");
+  console.log("A");
+  console.log("H");
+  console.log("I");
+  console.log("L");
+}
+
+// sayMyname();
+
+function AddNumbers(num1, num2) {
+  //   let result = num1 + num2;
+  //   return result;
+
+  return num1 + num2;
+}
+
+// AddNumbers(3, 4);
+let result = AddNumbers(10, 10);
+// console.log(result);
+
+function loginUserMessage(userame = "unknown") {
+  if (userame === undefined) {
+    // console.log("please enter usename");
+    return;
+  }
+  return `${userame} jut logged in `;
+}
+let message = loginUserMessage("sahil");
+// console.log(message);
+
+function CalculateCartPrice(...num) {
+  return num;
+}
+
+// console.log(CalculateCartPrice(100, 200, 300, 400));
+
+const product = {
+  name: "keyboard",
+  price: 500,
+};
+
+function handleObject(anyobject) {
+  console.log(`product is ${anyobject.name} and price is ${anyobject.price}`);
+}
+
+// console.log(handleObject(product));
+
+const myNewArray = [100, 200, 300];
+
+function returnArray(anyarray) {
+  return anyarray[2];
+}
+
+console.log(returnArray([100,200,4000,3000]));
