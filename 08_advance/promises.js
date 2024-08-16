e3be506ef2f0f3fa38b0a3017099158f458a5a04
sahil030804 <sahilranpariya60@gmail.com>
2024-08-16 12:30:50 +0000

    newest

--- 08_advance/promises.js
+++ 08_advance/promises.js
@@ -31,3 +31,44 @@ const promiseThree = new Promise(function (resolve, reject) {
 promiseThree.then(function (user) {
   console.log(user);
 });
+
+//////////////////////////////////////////////
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve({ username: "sahil", password: "12345" });
+    } else {
+      reject("ERROR: something went wrong");
+    }
+  }, 1000);
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("the promise is either resolved or rejected");
+  });
+
+////////////////////////////////////////////////////
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
