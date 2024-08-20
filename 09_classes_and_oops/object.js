a685e1bb468344eae2af48c15b72dfe55f9af4ab
sahil030804 <sahilranpariya60@gmail.com>
2024-08-20 07:53:30 +0000

    objects

--- 09_classes_and_oops/object.js
+++ 09_classes_and_oops/object.js
@@ -5,3 +5,24 @@ muliply5.power = 2;
 console.log(muliply5(5));
 console.log(muliply5.power);
 console.log(muliply5.prototype);
+
+function createUser(username, score) {
+  this.username = username;
+  this.score = score;
+}
+
+createUser.prototype.increment = function () {
+  this.score++;
+};
+createUser.prototype.printMe = function () {
+  console.log(`price  is ${this.score} `);
+};
+
+const chai = new createUser("chai", 25);
+const tea = new createUser("tea", 250);
+
+console.log(chai);
+console.log(tea);
+
+chai.printMe();
+tea.printMe();
